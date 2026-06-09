4b53b3fdce198242b0172f6923ca32a496c5313f
Irene Hofstetter <irene.hofstetter@npower.ch>
2026-06-09 22:22:17 +0200

    Learned Lesson 5
    Co-authored-by: Ronald Rink <dfch@users.noreply.github.com>

--- lesson04/data_types_ih.py
+++ lesson04/data_types_ih.py
@@ -7,3 +7,10 @@ print(type(last))
 print(isinstance(first, str))
 print(isinstance(last, str))
 
+# Declare and initialise a variable with "type hints":
+BEST_PRICE: int = 80
+
+# Declare and initialise a variable with "constructor":
+SOME_PRICE = int(80)
+
+FUTURE_PRICE: int | None = None
--- lesson05/rps.py
+++ lesson05/rps.py
@@ -1,6 +1,8 @@
 import sys
 import random
-from enum import Enum
+from enum import Enum, IntEnum, auto
+
+long_line = "This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. This is a very long line. "
 
 
 class RPS(Enum):
@@ -9,11 +11,26 @@ class RPS(Enum):
     SCISSORS = 3
 
 
+class RPS_BETTER(IntEnum):
+    ROCK = auto()
+    PAPER = auto()
+    SCISSORS = auto()
+    # WELL = auto()
+
+
 print("")
-playerchoice = input(
-    "Enter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
+playerchoice = input("Enter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
+
+# player_choice = "Enter ...\n"
+# for item in RPS:
+#     player_choice = f"{player_choice}{item.value} for {item.name},\n"
+# player_choice = f"{player_choice}\n"
 
 player = int(playerchoice)
+
+# allowed_values = [item.value for item in RPS]
+# if player not in allowed_values:
+#     sys.exit(f"You must enter one of these values: {allowed_values}")
 
 if player < 1 or player > 3:
     sys.exit("You must enter 1, 2, or 3.")
@@ -23,12 +40,12 @@ computerchoice = random.choice("123")
 computer = int(computerchoice)
 
 print("")
-print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
-print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+print("You chose " + str(RPS(player)).replace("RPS.", "") + ".")
+print("Python chose " + str(RPS(computer)).replace("RPS .", "") + ".")
 print("")
 
 if player == 1 and computer == 3:
-    print("🎉 You win!")
+    print("🎉 You win! 💕")
 elif player == 2 and computer == 1:
     print("🎉 You win!")
 elif player == 3 and computer == 2:
