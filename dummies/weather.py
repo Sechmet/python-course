6941bd86ff9f03015a93c399bd48faad32469ef1
Irene Hofstetter <irene.hofstetter@npower.ch>
2026-07-14 20:45:25 +0200

    added another constant to code weather.py

--- dummies/weather.py
+++ dummies/weather.py
@@ -2,16 +2,18 @@
 # A beginner-friendly script to get started with weather data
 
 
-TEMPERATURE = 5
+TEMPERATURE = 30
 WATER_TEMPERATURE = 24
 WIND_SPEED = 8
 
 
 GO_SWIM = TEMPERATURE > 25 and WATER_TEMPERATURE > 20 and WIND_SPEED < 10
 GO_WALK = TEMPERATURE > 20 and WIND_SPEED < 10
+STAY_INSIDE = not (GO_SWIM or GO_WALK)
 
 print(f"go swim: {GO_SWIM}")
 print(f"go walk: {GO_WALK}")
+print(f"stay_inside: {STAY_INSIDE}")
 
 if TEMPERATURE >= 20 and WIND_SPEED <= 10:
     weather = "good"
